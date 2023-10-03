5b2ca8845762b9d24da398ab6e048969f0761dc3
morij <cy19264@shibaura-it.ac.jp>
2023-10-04 01:54:40 +0900

    m

--- Main.java
+++ Main.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
 public class Main{
     public static void main(String args[]){
-        System.out.println("Hello, World");
-    }
+        System.out.println("®”‚ğ“ü—Í‚µ‚Ä‚­‚¾‚³‚¢");
+        Scanner scanner = new Scanner(System.in);
+        String scan = scanner.next();
+        int num=Integer.parseInt(scan);
+        if(num%2==0){
+            System.out.println("even");
+        }else{
+            System.out.println("odd");
+        }
+        scanner.close();
+     }
 }
